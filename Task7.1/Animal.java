97fc7f9e2c8a2f86312e9f62718595b92881b738
DhafinRizqiF <95175164+DhafinRizqiF@users.noreply.github.com>
2022-04-11 12:49:06 +0700

    Add files via upload

---
+++ Task7.1/Animal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Task1;
+
+/**
+ *
+ * @author Dhafin
+ */
+public class Animal {
+    public void sound(){
+        System.out.println("Animal Is making a sound");
+    }  
+}
